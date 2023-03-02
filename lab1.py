7f6286520147215c2babad75bab9f459b775b881
xegrimoth <xegrimoth@gmail.com>
2023-03-02 14:01:45 +0200

    Commit 1

---
+++ lab1.py
@@ -0,0 +1,18 @@
+import requests
+
+url = input()
+
+def more(text):
+    count = 0
+    for line in text.split('\n'):
+        print(line)
+        count += 1
+        if count % 30 == 0:
+            reply = input('Show more (y/n)? ')
+            if reply == 'n':
+                break
+
+with requests.get(url) as response:
+    html = response.text
+#    print("Web Server: " + response.headers.get("Server"))
+    print(response.headers)
